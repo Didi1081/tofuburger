886264f9682edfa59ad03e6ca8a1ed2e3d6e4b8c
Didi <ddapp108@gmail.com>
2018-11-26 13:58:54 -0600

    styles

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,30 @@
 $('#submit').on('click', function(){
-    alert('this works');
+   
     var newBurger = {
         name: $('#burgerInput').val(),
-        devoured: 1
+        devoured: 0
     }
     $.post('/api/burgers', newBurger, function(response){
         console.log(response);
+        window.location = '/';
     })
 })
+
+$(document).on('click', '.eat', function(){
+    var burgerId = $(this).attr('data-value');
+
+    $.ajax({
+        type: 'PUT',
+        url: '/api/burgers/'+ burgerId,
+        data: {devoured: 1}
+    }).then(function(res){
+        console.log(res, "this is the reuslt");
+        window.location = '/';
+    });
+});
+
+
+
+
+
+
